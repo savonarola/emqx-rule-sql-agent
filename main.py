3604e88c4066a653fe6af94f87efac16ae5f9e99
Ilya Averyanov <av@rubybox.dev>
2025-04-17 16:19:23 +0300

    improve interface

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 from pathlib import Path
 
 import agents
+import tiktoken
 from agents import Agent, Runner
 from agents.mcp import MCPServer, MCPServerStdio
 from prompt_toolkit import PromptSession
@@ -14,10 +15,14 @@ logging.getLogger("openai.agents").setLevel(logging.WARNING)
 agents.run.RunConfig.tracing_disabled = True
 
 
-def instructions():
-    doc_file_pattern = Path(__file__).parent.joinpath("rule-sql-context").joinpath("*.md")
-    print(doc_file_pattern)
+def instructions(docs_dir: str):
+    doc_file_pattern = Path(docs_dir) / "*.md"
     rule_sql_doc_files = glob.glob(str(doc_file_pattern))
+
+    print("Loading docs for context:")
+    for f in rule_sql_doc_files:
+        print(f)
+
     rule_sql_docs = [open(f).read() for f in rule_sql_doc_files]
     syntax = "\n".join(rule_sql_docs)
 
@@ -26,6 +31,8 @@ def instructions():
 
     The following are the documents for the SQL statements:
     {syntax}
+
+    Use the validate_sql tool to validate the SQL statement before providing the final answer.
     """
 
 
@@ -34,20 +41,21 @@ def extend_prompt(user_input: str):
     Please help to compose an SQL statement for the EMQX Rule Engine:
     {user_input}
 
-    Use the validate_sql tool to validate the SQL statement before providing the final answer.
-    Use the 
+    Call validate_sql tool with samples that are expected to match
+    and also with samples that are expected to not match the SQL statement.
 
-    For validation with validate_sql tool, provide samples that are expected to match
-    and samples that are expected to not match the SQL statement.
+    In case of 'SQL Not Match' error try different topic names or different WHERE conditions.
 
     In case of composing an SQL statement, provide concise answer, without any additional text.
     """
-
 
 async def run(mcp_server: MCPServer, instructions: str, args: argparse.Namespace):
     session = PromptSession(
         message="rule-sql-agent> "
     )
+
+    if args.count_tokens:
+        print("Instruction size", count_tokens(instructions, args))
 
     # Get all the topics from the EMQX server
     agent = Agent(
@@ -72,13 +80,14 @@ def main():
     parser = argparse.ArgumentParser("EMQX Rule SQL Agent")
     parser.add_argument("--model", type=str, default="gpt-4o")
     parser.add_argument("--emqx-mcp-server-dir", type=dir_path, required=True)
+    parser.add_argument("--docs-dir", type=dir_path, required=True)
     parser.add_argument("--emqx-api-url", type=str, default="http://localhost:18083/api/v5")
     parser.add_argument("--emqx-api-key", type=str, default="key")
     parser.add_argument("--emqx-api-secret", type=str, default="secret")
-
+    parser.add_argument("--count-tokens", default=False, action="store_true")
     args = parser.parse_args()
     try:
-        asyncio.run(run_mcp_server(instructions(), args))
+        asyncio.run(run_mcp_server(instructions(args.docs_dir), args))
     except KeyboardInterrupt:
         print("Goodbye!")
 
@@ -109,6 +118,10 @@ def dir_path(path_str):
         return str(path)
     else:
         raise argparse.ArgumentTypeError(f"'{path_str}' is not a valid directory")
+
+def count_tokens(text: str, args: argparse.Namespace):
+    encoding = tiktoken.encoding_for_model(args.model)
+    return len(encoding.encode(text))
 
 if __name__ == "__main__":
     main()
